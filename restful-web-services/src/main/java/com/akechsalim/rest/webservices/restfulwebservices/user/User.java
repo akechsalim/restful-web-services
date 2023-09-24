7bfee39ba10161a59ba6b694174d4b0224c88347
akechsalim <akechsalimwork@gmail.com>
2023-09-24 15:25:46 +0300

    added repository class

--- restful-web-services/src/main/java/com/akechsalim/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/akechsalim/rest/webservices/restfulwebservices/user/User.java
@@ -1,16 +1,31 @@
 package com.akechsalim.rest.webservices.restfulwebservices.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
+@Entity(name = "User_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
+    @Size(min = 2, message = "Name should have at least 2 characters")
     private String name;
+    @Past(message = "Birth date should be in the past ")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
+    }
+
+    public User() {
+
     }
 
     public Integer getId() {
--- restful-web-services/src/main/java/com/akechsalim/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/akechsalim/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.akechsalim.rest.webservices.restfulwebservices.user;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -26,7 +27,7 @@ public class UserResource {
     }
 
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
+    public void createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}")
