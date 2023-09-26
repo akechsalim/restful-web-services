2a11af138fd7768b2679392d128720cefeab792b
akechsalim <akechsalimwork@gmail.com>
2023-09-26 10:39:02 +0300

    removed unused package and made changes to pom.xml.

--- restful-web-services/src/main/java/com/akechsalim/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/akechsalim/rest/webservices/restfulwebservices/user/User.java
@@ -1,7 +1,6 @@
 package com.akechsalim.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -18,10 +17,8 @@ public class User {
     @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 characters")
-//    @JsonProperty("user_name")
     private String name;
     @Past(message = "Birth date should be in the past ")
-//    @JsonProperty("birth_date")
     private LocalDate birthDate;
     @OneToMany(mappedBy = "user")
     @JsonIgnore
